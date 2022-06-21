31860d8af2612f3666fba11762aa0d62bec31a22
prince-ao <princead@tutanota.com>
2022-06-21 18:23:42 -0400

    FIX: Fixed redoc

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -2,7 +2,78 @@ import { RedocStandalone } from 'redoc';
 
 function App() {
   return (
-    <RedocStandalone specUrl="http://petstore.swagger.io/v2/swagger.json" />
+    <RedocStandalone spec={{
+      "openapi": "3.0.1",
+      "info": {
+        "title": "TheCodeBuzz-Service",
+        "version": "v1"
+      },
+      "paths": {
+        "/api/Pay": {
+          "get": {
+            "tags": [
+              "Pay"
+            ],
+            "responses": {
+              "200": {
+                "description": "Success",
+                "content": {
+                  "text/plain": {
+                    "schema": {
+                      "$ref": "#/components/schemas/Employee"
+                    }
+                  },
+                  "application/json": {
+                    "schema": {
+                      "$ref": "#/components/schemas/Employee"
+                    }
+                  },
+                  "text/json": {
+                    "schema": {
+                      "$ref": "#/components/schemas/Employee"
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      },
+      "components": {
+        "schemas": {
+          "Employee": {
+            "type": "object",
+            "properties": {
+              "id": {
+                "type": "string",
+                "nullable": true,
+                "readOnly": true
+              },
+              "name": {
+                "type": "string",
+                "nullable": true,
+                "readOnly": true
+              }
+            },
+            "additionalProperties": false
+          }
+        },
+        "securitySchemes": {
+          "bearerAuth": {
+            "type": "http",
+            "description": "JWT Authorization header using the Bearer scheme.",
+            "scheme": "bearer",
+            "bearerFormat": "JWT"
+          }
+        }
+      },
+      "security": [
+        {
+          "bearerAuth": []
+        }
+      ]
+
+    }} />
   );
 }
 
